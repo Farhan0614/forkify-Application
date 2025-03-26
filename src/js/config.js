fdcddfd05829e1319cf9d68c8d18a58ee24a67fd
Farhan0614 <farhanmirza0614@gmail.com>
2025-03-26 22:13:19 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dac4893a-655f-4c17-9daa-860638496b14';
+export const MODAL_CLOSE_SEC = 2.5;
